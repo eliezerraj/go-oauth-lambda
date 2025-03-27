c68b06bd4917a6ba4cd62e5c710711a3b1881061
eliezerraj <eliezerraj@gmail.com>
2025-03-27 20:35:46 -0300

    feat(workload):new health

--- cmd/main.go
+++ cmd/main.go
@@ -165,6 +165,8 @@ func main (){
 	if err != nil {
 		panic("error create new aws session " + err.Error())
 	}
+
+	// Otel over aws services
 	otelaws.AppendMiddlewares(&awsConfig.APIOptions)
 	
 	// Prepare AWS services
